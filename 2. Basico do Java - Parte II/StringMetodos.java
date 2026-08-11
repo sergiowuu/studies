5c4d3e0bad95985a4fec9803791f0d3e0a6d35ae
Sergio Wu <srwu00@gmail.com>
2026-08-11 19:18:08 -0300

    String e metodos

---
+++ 2. Basico do Java - Parte II/StringMetodos.java
@@ -0,0 +1,8 @@
+public class StringMetodos {
+    public static void main(String[] args) {
+        String emailDigitado = "  Sergio@Email.COM  ";
+        String emailHigienizado = emailDigitado.trim().toLowerCase();
+        System.out.println("Email original: " + emailDigitado);
+        System.out.println("Email higienizado: " + emailHigienizado);
+    }
+}
