226f79c1ee032e58b3c1a692a84834f3680f0e74
Sergio Wu <srwu00@gmail.com>
2026-08-21 20:39:48 -0300

    Atributos e métodos

---
+++ src/poo/atributosMetodos/Main.java
@@ -0,0 +1,12 @@
+package poo.atributosMetodos;
+
+public class Main {
+    public static void main(String[] args) {
+        Retangulo retangulo = new Retangulo();
+
+        retangulo.largura = 5.0;
+        retangulo.altura = 3.0;
+
+        retangulo.exibirDados();
+    }
+}
---
+++ src/poo/atributosMetodos/Retangulo.java
@@ -0,0 +1,21 @@
+package poo.atributosMetodos;
+
+public class Retangulo {
+    double largura;
+    double altura;
+
+    public double calcularArea() {
+        return largura * altura;
+    }
+
+    public double calcularPerimetro() {
+        return 2 * (largura + altura);
+    }
+
+    public void exibirDados() {
+        System.out.println("Largura: " + largura);
+        System.out.println("Altura: " + altura);
+        System.out.println("Área: " + calcularArea());
+        System.out.println("Perímetro: " + calcularPerimetro());
+    }
+}
