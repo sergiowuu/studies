40fb725acaecf727d8b878daeb1a1ecb02e8408c
Sergio Wu <srwu00@gmail.com>
2026-08-30 21:04:35 -0300

    Pass by value / pass by reference

---
+++ src/poo/passByValueReference/Main.java
@@ -0,0 +1,29 @@
+package poo.passByValueReference;
+
+public class Main {
+    public static void main(String[] args){
+        Relatorio relatorio = new Relatorio();
+
+        relatorio.setNome("Relatorio 1");
+        relatorio.setSaldo(10);
+        System.out.println("Saldo inicial: " + relatorio.getSaldo());
+
+        zerarSaldo(relatorio);
+        System.out.println("Saldo após zerar a primeira vez: " + relatorio.getSaldo());
+
+        relatorio.setSaldo(20);
+        System.out.println("Setando outro valor para saldd: " + relatorio.getSaldo());
+
+        zerarSaldoErrado(relatorio);
+        System.out.println("Saldo após zerar errado: " + relatorio.getSaldo());
+    }
+
+    static void zerarSaldo(Relatorio relatorio){
+        relatorio.setSaldo(0);
+    }
+
+    static void zerarSaldoErrado(Relatorio relatorio){
+        relatorio = new Relatorio();
+        relatorio.setSaldo(0);
+    }
+}
---
+++ src/poo/passByValueReference/Relatorio.java
@@ -0,0 +1,22 @@
+package poo.passByValueReference;
+
+public class Relatorio {
+    private String nome;
+    private double saldo;
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public double getSaldo(){
+        return saldo;
+    }
+
+    public void setSaldo(double saldo){
+        this.saldo = saldo;
+    }
+}
