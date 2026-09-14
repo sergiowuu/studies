3c7ec027cd282f04e76c81ce1c34d5d28b46cc1b
Sergio Wu <srwu00@gmail.com>
2026-09-14 20:50:19 -0300

    ArrayList

---
+++ src/ArrayList/InscricaoEvento.java
@@ -0,0 +1,35 @@
+package ArrayList;
+
+import java.util.ArrayList;
+
+public class InscricaoEvento {
+    private ArrayList<String> participantes = new ArrayList<>();
+
+    public String inscreverParticipante(String nome){
+        if (candidatoInscrito(nome)){
+            return "Candidato já inscrito";
+        } else {
+            participantes.add(nome);
+            return "Participante inscrito";
+        }
+    }
+
+    public String cancelarInscricao(String nome){
+        if (candidatoInscrito(nome)){
+            participantes.remove(nome);
+            return "Participante removido";
+        } else {
+            return "Participante não está inscrito";
+        }
+    }
+
+    public boolean candidatoInscrito(String nome){
+        return participantes.contains(nome);
+    }
+
+    public void listarParticipantes(){
+        for (String participante : participantes){
+            System.out.println(participante);
+        }
+    }
+}
---
+++ src/ArrayList/Main.java
@@ -0,0 +1,21 @@
+package ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        InscricaoEvento inscricaoEvento = new InscricaoEvento();
+
+        System.out.println(inscricaoEvento.inscreverParticipante("Paulão"));
+        System.out.println(inscricaoEvento.inscreverParticipante("Paulão"));
+        System.out.println(inscricaoEvento.inscreverParticipante("Julieto"));
+        System.out.println(inscricaoEvento.inscreverParticipante("Marcola"));
+        System.out.println(inscricaoEvento.inscreverParticipante("Travis"));
+
+        System.out.println(inscricaoEvento.cancelarInscricao("Pedro"));
+        System.out.println(inscricaoEvento.cancelarInscricao("Paulão"));
+
+        System.out.println(inscricaoEvento.candidatoInscrito("Paulão"));
+        System.out.println(inscricaoEvento.candidatoInscrito("Marcola"));
+
+        inscricaoEvento.listarParticipantes();
+    }
+}
